3a4507d4c3d0697358130bb16d498bebb732d35f
Laura Manenti <Lallina@lauras-air.homenet.telecomitalia.it>
2018-05-08 01:45:02 +0200

    modified plotPurityVSfield.C

--- plotPurityVSfield.C
+++ plotPurityVSfield.C
@@ -13,48 +13,113 @@
 // int colors[]       = {kGray+2,  kBlue+1, kCyan+1, kGreen+1,  kRed, kOrange+1, kViolet+1};   
 
 
-string basename="2018May02liquefaction/liquid/";
+//string basename="2018May02liquefaction/liquid/";
+//string fields[]    = {
+//  //  "K-43GK-25GA25A45_10.10.20Vcm"      ,
+//  "K-68GK-50GA50A90_10.20.40Vcm"     , 
+//  "K-136GK-100GA100A180_20.40.80Vcm",
+//  "K-170GK-125GA125A225_25.50.100Vcm", 
+//  "K-204GK-150G150GA270_30.60.120Vcm", 
+//  // "K-204GK-150G150GA270_30.60.120Vcm", 
+//  "K-272GK-200GA200A360_40.80.160Vcm", 
+//  "K-340GK-250GA250A450_50.100.200Vcm",
+//  // "K-475GK-350GA350A630_70.140.280Vcm",
+//  // "K-475GK-350GA350A630_70.140.280Vcm",
+//  "K-475GK-350GA350A630_70.140.280Vcm"
+//};
+//string divisions[] = {
+//  //  "50mVdiv_19.35",
+//  "50mVdiv_19.11",
+//  "50mVdiv_18.53",
+//  "100mVdiv_18.38",
+//  "50mVdiv_18.15",
+//  // "100mVdiv_18.09",
+//  "100mVdiv_17.49_ampSwitch",
+//  "100mVdiv_17.30_ampSwitch",
+//  // "100mVdiv_16.54",
+//  // "200mVdiv_16.59",
+//  "200mVdiv_17.10_ampSwitch"
+//};
+//
+//string fieldNice[] = {
+//  //  "10-10-20 V/m",
+//  "10-20-40 V/m",
+//  "20-40-80 V/m",
+//  "25-50-100 V/m",
+//  "30-60-120 V/m",
+//  "30-60-120 V/m",
+//  "40-80-160 V/m",
+//  "50-100-200 V/m",
+//  // "70-140-280 V/m",
+//  // "70-140-280 V/m",
+//  "70-140-280 V/m"
+//};
+
+string basename="2018May02liquefaction/liquidDay2/";
 string fields[]    = {
-  //  "K-43GK-25GA25A45_10.10.20Vcm"      ,
-  "K-68GK-50GA50A90_10.20.40Vcm"     , 
-  "K-136GK-100GA100A180_20.40.80Vcm",
-  "K-170GK-125GA125A225_25.50.100Vcm", 
-  "K-204GK-150G150GA270_30.60.120Vcm", 
-  // "K-204GK-150G150GA270_30.60.120Vcm", 
-  "K-272GK-200GA200A360_40.80.160Vcm", 
-  "K-340GK-250GA250A450_50.100.200Vcm",
-  // "K-475GK-350GA350A630_70.140.280Vcm",
-  // "K-475GK-350GA350A630_70.140.280Vcm",
-  "K-475GK-350GA350A630_70.140.280Vcm"
+  "K-680GK-500GA500A900_100.200.400cm",
+  "K-680GK-500GA500A900_100.200.400cm",
+  "K-680GK-500GA500A900_100.200.400cm",
+  "K-680GK-500GA500A900_100.200.400cm",
+  "K-612GK-450GA450A810_90.180.360cm",
+  "K-544GK-400GA400A720_80.160.320cm",
+  "K-476GK-350GA350A630_70.140.280cm",
+  "K-476GK-350GA350A630_70.140.280cm",
+  "K-476GK-350GA350A630_70.140.280cm",
+  "K-476GK-350GA350A630_70.140.280cm",
+  "K-476GK-350GA350A630_70.140.280cm",
+  "K-340GK-250GA250A450_50.100.200cm",
+  "K-272GK-200GA200A360_40.80.160cm",
+  "K-680GK-500GA500A900_100.200.400cm",
+  "K-1530GK-1125GA1125A2025_225.450.900cm",
+  "K-1360GK-1000GA1000A1800_200.400.800cm",
+  "K-1020GK-750GA750A1350_150.300.600cm"
+  
 };
 string divisions[] = {
-  //  "50mVdiv_19.35",
-  "50mVdiv_19.11",
-  "50mVdiv_18.53",
-  "100mVdiv_18.38",
-  "50mVdiv_18.15",
-  // "100mVdiv_18.09",
-  "100mVdiv_17.49_ampSwitch",
-  "100mVdiv_17.30_ampSwitch",
-  // "100mVdiv_16.54",
-  // "200mVdiv_16.59",
-  "200mVdiv_17.10_ampSwitch"
+  "100mVdiv_16.43",
+  "100mVdiv_16.25",
+  "100mVdiv_15.51",
+  "100mVdiv_15.26",
+  "100mVdiv_13.23",
+  "100mVdiv_13.38",
+  "100mVdiv_16.35",
+  "100mVdiv_16.13",
+  "100mVdiv_16.02",
+  "100mVdiv_15.13",
+  "100mVdiv_13.50",
+  "100mVdiv_14.07",
+  "100mVdiv_14.20",
+  "100mVdiv_13.10",
+  "200mVdiv_12.34",
+  "1Vdiv_12.13",
+  "200mVdiv_12.46"
 };
 
 string fieldNice[] = {
-  //  "10-10-20 V/m",
-  "10-20-40 V/m",
-  "20-40-80 V/m",
-  "25-50-100 V/m",
-  "30-60-120 V/m",
-  "30-60-120 V/m",
-  "40-80-160 V/m",
-  "50-100-200 V/m",
-  // "70-140-280 V/m",
-  // "70-140-280 V/m",
-  "70-140-280 V/m"
+  "100-200-400 V/cm",
+  "100-200-400 V/cm",
+  "100-200-400 V/cm",
+  "100-200-400 V/cm",
+  
+  "90-180-360 V/cm",
+  "80-160-320 V/cm",
+  "70-140-280 V/cm",
+  "70-140-280 V/cm",
+  "70-140-280 V/cm",
+  "70-140-280 V/cm",
+  "70-140-280 V/cm",
+  
+  "50-100-200 V/cm",
+  "40-80-160 V/cm",
+  "100-200-400 V/cm",
+  
+  "225-450-900 V/cm",
+  "200-400-800 V/cm",
+  "150-300-600 V/cm"
 };
-int colors[]       = {kGray+2,  kBlue+1, kCyan+1, kGreen+1,  kRed, kOrange+1, kViolet+1, kBlue+2, kBlue+3, kBlue+5, kBlue+9};   
+
+int colors[]       = {kGray+2,  kBlue+1, kCyan+1, kGreen+1,  kRed, kOrange+1, kViolet+1, kBlue+2, kBlue+3, kBlue+5, kBlue+9};
 
 string whichAvg[4] = {"justAvg", "filteredAvg", "zeroedAvg", "fancyFilteredAvg"};
 string chnamenice[2] = {"anode", "Cathode"};
@@ -123,18 +188,19 @@ void plotPurityVSfield(){
     graph[3]->SetPoint(ifield, ifield, QA);
     graph[4]->SetPoint(ifield, ifield, QK);
     graph[5]->SetPoint(ifield, ifield, R);
-    graph[6]->SetPoint(ifield, ifield, purity);
+    graph[6]->SetPoint(ifield, ifield, purity*1E6);
     graph[7]->SetPoint(ifield, ifield, purity2);
     
   }
 
   TCanvas *c1 = new TCanvas("c1");
 
-  for (int ip=0; ip<nplots; ip++){
+  for (int ip=0; ip<nplots-1; ip++){
     graph[ip]->SetTitle(Form("%s;;%s", plots[ip].c_str(), plots[ip].c_str()));
     graph[ip]->GetHistogram()->GetXaxis()->Set(numFields, -0.5, numFields-0.5);
     for (int i=1;i<=numFields;i++) graph[ip]->GetHistogram()->GetXaxis()->SetBinLabel(i,fieldNice[i-1].c_str());
     graph[ip]->Draw("Al text");
+    c1->SetGrid();
     c1->Print((basename+"/plots/Allfields_"+plots[ip]+".png").c_str());
   }
   
